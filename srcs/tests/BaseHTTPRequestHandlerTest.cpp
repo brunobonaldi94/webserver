f14fd2c85c4331f8da6305d581b4edadb4d2ae71
brunobonaldi94 <brunobonaldi94@gmail.com>
2023-12-25 19:18:19 -0300

    test: add tests for different request: chunk, regular body, multipart data

--- srcs/tests/BaseHTTPRequestHandlerTest.cpp
+++ srcs/tests/BaseHTTPRequestHandlerTest.cpp
@@ -1,29 +1,6 @@
 #include "doctest.h"
 #include "RequestHandler.hpp"
 
-TEST_CASE("SENDER HEADER")
-{
-    ADirectoryHandler *directoryHandler = new DirectoryHandler();
-    RequestHandler requestHandler(directoryHandler);
-    requestHandler.sendHeader("Cache-Control", "no-cache, private");
-	requestHandler.sendHeader("Content-type", "text/html");
-
-    CHECK(requestHandler.headersBufferToString() == \
-        "Cache-Control: no-cache, private\n" \
-        "Content-type: text/html\n");
-    CHECK(requestHandler.headersBufferToString() != \
-        "Cache-Control: no-cache, private\n" \
-        "Content-type: text/html\n\n");
-    CHECK(requestHandler.headersBufferToString() != "Cache-Control: no-cache, private");
-    CHECK(requestHandler.headersBufferToString() != "");
-
-    //send new header
-    requestHandler.sendHeader("Content-Length", "100");
-    CHECK(requestHandler.headersBufferToString() == \
-        "Cache-Control: no-cache, private\n" \
-        "Content-type: text/html\n" \
-        "Content-Length: 100\n");
-}
 
 TEST_CASE("parseRequestChuckedBody")
 {
@@ -44,8 +21,55 @@ TEST_CASE("parseRequestChuckedBody")
         "\r\n";
     ServerContext *serverContext = new ServerContext();
     ServerConfig *serverConfig = new ServerConfig(serverContext);
-    requestHandler.setClientSockerRequestContentMap(3, serverConfig);
-    requestHandler.parseRequest(request);
+    requestHandler.parseRequestForClientSocket(request, 3, serverConfig);
+    std::string body = requestHandler.getCurrentRequestContent()->getBody();
+    delete serverConfig;
+    delete serverContext;
+    CHECK(body == "hello world");
+}
+
+TEST_CASE("parseRegularBody")
+{
+    ADirectoryHandler *directoryHandler = new DirectoryHandler();
+    RequestHandler requestHandler(directoryHandler);
+    std::string request = "POST / HTTP/1.1\r\n" \
+        "Host: localhost:8080\r\n" \
+        "User-Agent: curl/7.64.1\r\n" \
+        "Accept: */*\r\n" \
+        "Content-Length: 11\r\n" \
+        "Content-Type: application/x-www-form-urlencoded\r\n" \
+        "\r\n" \
+        "hello world";
+    ServerContext *serverContext = new ServerContext();
+    ServerConfig *serverConfig = new ServerConfig(serverContext);
+    requestHandler.parseRequestForClientSocket(request, 4, serverConfig);
+    std::string body = requestHandler.getCurrentRequestContent()->getBody();
+    delete serverConfig;
+    delete serverContext;
+    CHECK(body == "hello world");
+}
+
+TEST_CASE("parseRequestMultipartData")
+{
+    ADirectoryHandler *directoryHandler = new DirectoryHandler();
+    RequestHandler requestHandler(directoryHandler);
+std::string bodyRequest = "\r\n--------------------------d1b6c5f7e0e5a8a8\r\n" \
+    "Content-Disposition: form-data; name=\"file\"; filename=\"test.txt\"\r\n" \
+    "Content-Type: text/plain\r\n" \
+    "\r\n" \
+    "hello world\r\n" \
+    "--------------------------d1b6c5f7e0e5a8a8--";
+
+std::string request = "POST / HTTP/1.1\r\n" \
+    "Host: localhost:8080\r\n" \
+    "User-Agent: curl/7.64.1\r\n" \
+    "Accept: */*\r\n" \
+    "Content-Length: " + StringUtils::ConvertNumberToString(bodyRequest.length()) + "\r\n" \
+    "Content-Type: multipart/form-data; boundary=------------------------d1b6c5f7e0e5a8a8\r\n" \
+    "\r\n" + bodyRequest;
+    ServerContext *serverContext = new ServerContext();
+    ServerConfig *serverConfig = new ServerConfig(serverContext);
+    requestHandler.parseRequestForClientSocket(request, 5, serverConfig);
     std::string body = requestHandler.getCurrentRequestContent()->getBody();
     delete serverConfig;
     delete serverContext;
